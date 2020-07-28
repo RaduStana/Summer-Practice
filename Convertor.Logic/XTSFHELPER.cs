e4135af3a0e6a201cdf7bfc4d5182ff95387ef9d
Stana Radu <stana.radu@gmail.com>
2020-07-28 12:17:35 +0300

    TimeSheet Week4

--- Convertor.Logic/XTSFHELPER.cs
+++ Convertor.Logic/XTSFHELPER.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Convertor.Model;
+using System;
 using System.IO;
+using System.Runtime.InteropServices.ComTypes;
 using System.Xml.Serialization;
 
 namespace Convertor.Logic
@@ -23,6 +25,32 @@ namespace Convertor.Logic
             {
                 xmlSerializer.Serialize(writer, anyobject);
             }
+        }
+
+        public static void CreateCalpFile()
+        {
+            var xTSF = DeserializeToObject<Testsetfile>(@"D:\ZF\SummerPractice.xtsf");
+            string text = "includes\n{\n}\nvariabels\n{\n}\nvoid testCase()\n{\n}" ;
+
+            foreach ( Entry entry in xTSF.Hash.Entry)
+            {
+                foreach ( Condition condition in entry.Condition)
+                {
+                    if(condition.Type.Equals("signal write"))
+                    {
+                        string separator = "=";
+                        int separatorIndex = condition.Value.IndexOf(separator);
+                        if (separatorIndex >= 0)
+                        {
+                            string resultName = condition.Value.Substring(0,separatorIndex);
+                            string resultValue = condition.Value.Substring(separatorIndex, separatorIndex+3);
+                            text = text + "if(setSignal(" + resultName + "," + resultValue + ")";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(text);
+            System.IO.File.WriteAllText(@"D:\ZF\SummerPractice.calp", text);
         }
     }
 }
--- Convertor/Program.cs
+++ Convertor/Program.cs
@@ -1,16 +1,30 @@
 ﻿using Convertor.Logic;
 using Convertor.Model;
 using System;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Convertor
 {
-    class Program
+    public partial class Program : Form
     {
+        public Program()
+        {
+            //InitializeComponent();
+        }
         static void Main(string[] args)
         {
-            var xTSF = XTSFHELPER.DeserializeToObject<Testsetfile>(@"D:\ZF\SummerPractice.xtsf");
+            /*var xTSF = XTSFHELPER.DeserializeToObject<Testsetfile>(@"D:\ZF\SummerPractice.xtsf");
             xTSF.Prename = "Summer";
             XTSFHELPER.SerializeToXml<Testsetfile>(xTSF, @"D:\ZF\SummerPractice2.xtsf");
+            */
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Program());
+            XTSFHELPER.CreateCalpFile();
+
         }
     }
 }
